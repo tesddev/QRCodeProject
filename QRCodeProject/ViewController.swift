5ccb966489035ca253f07c8d218ab37c21485f61
tesddev <tesleem.amuda@gmail.com>
2022-07-28 09:52:52 +0100

    create project basics

--- QRCodeProject/ViewController.swift
+++ QRCodeProject/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var QRCodeImageView: UIImageView!
 
+    @IBOutlet weak var detailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let text = "tesddev"
+        QRCodeImageView.image = generateQRCode(from: "https://github.com/\(text)")
+        detailLabel.text = text
+        
     }
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+
+        return nil
+    }
+
+//    let image = generateQRCode(from: "Hacking with Swift is the best iOS coding tutorial I've ever read!")
 
 
 }
